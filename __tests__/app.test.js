c0d517ce1a3183d6955ca6aac62f04a77af793f4
sformichella <sformichella@gmail.com>
2020-11-04 10:18:19 -0800

    add post route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,34 +31,69 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-  test('returns animals', async() => {
+    test('returns theorems', async() => {
 
-    const expectation = [
-      {
-        'id': 1,
-        'name': 'bessie',
-        'coolfactor': 3,
-        'owner_id': 1
-      },
-      {
-        'id': 2,
-        'name': 'jumpy',
-        'coolfactor': 4,
-        'owner_id': 1
-      },
-      {
-        'id': 3,
-        'name': 'spot',
-        'coolfactor': 10,
-        'owner_id': 1
-      }
-    ];
+      const expectation = [
+        {
+          id: 1,
+          name: 'Pythagorean Theorem',
+          difficulty: 1,
+          veracity: true,
+          field: 'geometry',
+          owner_id: 1,
+        },
+        {
+          id: 2,
+          name: 'Prime Number Theorem',
+          difficulty: 5,
+          veracity: true,
+          field: 'number theory',
+          owner_id: 1,
+        },
+        {
+          id: 3,
+          name: 'Mertenz Conjecture',
+          difficulty: 7,
+          veracity: false,
+          field: 'number theory',
+          owner_id: 1,
+        },
+        {
+          id: 4,
+          name: 'Dirichlet\'s Theorem',
+          difficulty: 6,
+          veracity: true,
+          field: 'number theory',
+          owner_id: 1,
+        }
+      ];
 
-    const data = await fakeRequest(app)
-      .get('/animals')
-      .expect('Content-Type', /json/)
-      .expect(200);
+      const data = await fakeRequest(app)
+        .get('/theorems')
+        .expect('Content-Type', /json/)
+        .expect(200);
 
-    expect(data.body).toEqual(expectation);
+      expect(data.body).toEqual(expectation);
+    });
+
+
+    test('returns a theorem', async() => {
+      const expectation = {
+        id: 1,
+        name: 'Pythagorean Theorem',
+        difficulty: 1,
+        veracity: true,
+        field: 'geometry',
+        owner_id: 1,
+      };
+
+      const data = await fakeRequest(app)
+        .get('/theorems/1')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
   });
 });
